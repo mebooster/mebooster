9a6c2db9f20701b4630691e1e3ebe48a5f63c807
mebooster <111131650+mebooster@users.noreply.github.com>
2023-04-14 10:09:09 +0800

    Update caltech256.py

--- mebooster/datasets/caltech256.py
+++ mebooster/datasets/caltech256.py
@@ -21,12 +21,6 @@ from torchvision.datasets.folder import ImageFolder, default_loader
 
 import mebooster.config as cfg
 
-__author__ = "Tribhuvanesh Orekondy"
-__maintainer__ = "Tribhuvanesh Orekondy"
-__email__ = "orekondy@mpi-inf.mpg.de"
-__status__ = "Development"
-
-
 class Caltech256(ImageFolder):
 
     def __init__(self, train=True, transform=None, target_transform=None):
