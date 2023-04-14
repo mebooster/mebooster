28f911d3efd73d3a5818dd8784b938d9698043d6
mebooster <111131650+mebooster@users.noreply.github.com>
2023-04-14 10:50:28 +0800

    Update config.py

--- mebooster/config.py
+++ mebooster/config.py
@@ -24,7 +24,6 @@ ZOO_URL = 'http://datasets.d2.mpi-inf.mpg.de/blackboxchallenge'
 # -------------- Dataset Stuff
 # IMAGENET_MEAN = [0.485, 0.456, 0.406]
 # IMAGENET_STD = [0.229, 0.224, 0.225]
-
 IMAGENET_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STD = [0.5, 0.5, 0.5]
 
@@ -33,46 +32,27 @@ epoch = 1 #150
 k = 2000
 num_iter = 1
 over_factor = 5
-layer_by_layer = False#True; Not use
 attack_set = 'maze_ini'
 
-use_default_initial = False#True#False
+use_default_initial = False#True
 
 start_shadow_test = 0
 start_shadow_out_test = 10000
 
 initial_seed = 10000
-
-#shadow model(for a original 10000)
-start_shadow = 0
-start_shadow_out = 35000
-shadow_data = 20000
-read_shadow_from_path = False
-read_attack_mia_model_from_path = True
-
 log_interval = 2000
-imp_vic_mem = False #attack2
-imp_vic_mem_bound = False
-vic_mem_method = 'shadow_model'#'unsupervised' #shadow modell,
 
-transfer_mi = False
-augment_mi = False #only need the first 3000
-
-mi_test = True #test process in shadow model
-
-trainingbound = 50000
-
-# -------------- put config here
 test_dataset = 'CIFAR10'#'MNIST'#'CIFAR10' #'FashionMNIST'
 attack_model_arch ='over_resnet18'#'over_another_lenet'#'over_lenet5'#'over_resnet18'
 victim_model_arch ='resnet18'# 'lenet_tl'#'resnet18'
 queryset ='EMNIST,MNIST'#'CIFAR10-0,CIFAR100-0,DownSampleImagenet32-50000'#'
 attack_model_dir = osp.join(MODEL_DIR, "adversary\\ADV_DIR")
 transfer_set_out_dir = osp.join(MODEL_DIR, "adversary\\TRANSFER_SET")
-shadow_model_dir = osp.join(MODEL_DIR, "shadow")
-sampling_method = 'membership_attack'#'kcenter' #'membership_attack'#'random' #'kcenter' #uncetainty # adversarial #'membership_attack'
-ma_method = ''# bayes, generative, gradient, unsupervised, shadow_model
 seed = 1337
 copy_one_hot = False #use softmax; true: use copy_one_hot
 
+shadow_model_dir = osp.join(MODEL_DIR, "shadow")
+sampling_method = ''
+ma_method = ''
 unsuper_data = 1000
+trainingbound = 50000
