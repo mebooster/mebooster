1a2089e0cc9fd5335e53941fd4804e099c48edc1
mebooster <111131650+mebooster@users.noreply.github.com>
2023-04-14 10:12:02 +0800

    Update tinyimagenet200.py

--- mebooster/datasets/tinyimagenet200.py
+++ mebooster/datasets/tinyimagenet200.py
@@ -7,11 +7,6 @@ from torchvision.datasets import ImageFolder
 
 import mebooster.config as cfg
 
-__author__ = "Tribhuvanesh Orekondy"
-__maintainer__ = "Tribhuvanesh Orekondy"
-__email__ = "orekondy@mpi-inf.mpg.de"
-__status__ = "Development"
-
 
 class TinyImageNet200(ImageFolder):
     """
